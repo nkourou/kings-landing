640ad5633935c624c12fc9c685dc59cf15e8a635
Ayush Singh <singhay@users.noreply.github.com>
2023-10-29 14:43:44 -0700

    Post to slack instead of Zapier (#8)
    * Mute blog for now
    
    * move out into a dir
    
    * Mute tech section; remove gcms gql packages
    
    * Delete pckg lock
    
    * Remove reveal scroll plugin
    
    * Replace reveal scroll plugin with fixed version
    
    * Add netlify toml to support functions
    
    * Add netlify plugin to package json
    
    * Add encoding package
    
    * Alert on slack instead of Zapier
    
    * Post to slack
    
    ---------
    
    Co-authored-by: Ayush Singh <ayush.singh@evicore.com>

--- src/api/signup-form.ts
+++ src/api/signup-form.ts
@@ -10,36 +10,36 @@ export default async function handler(
     res: GatsbyFunctionResponse
 ) {
     if (!req.body.email) {
-        return res.status(422).json("Email field is required")
+        return res.status(422).send("Email field is required")
     }
 
     // POST data to Zapier who sends it to Google sheet & Slack 
     // TODO: trigger Poshmarkmapp email template send
-    const url = process.env.ZAPIER_WZ_WEBHOOK_URL!
+    const url = process.env.SLACK_WZ_WEBHOOK_URL!
     const headers = {
         // "Access-Control-Allow-Origin": "https://wuruzeka.com",
         "Content-Type": "application/json"
     }
-    // const data = {
-    //     channel: "#users",
-    //     text: `New user signed up for beta: ${req.body.email}`
-    // }
-    // https://api.slack.com/reference/surfaces/formatting#date-formatting
     const data = {
-        datetime: Date.now(),
-        date: new Date(Date.now()).toUTCString(),
-        email: req.body.email
+        text: `New user signed up for beta: ${req.body.email}`
     }
+    // https://api.slack.com/reference/surfaces/formatting#date-formatting
+    // const data = {
+    //     datetime: Date.now(),
+    //     date: new Date(Date.now()).toUTCString(),
+    //     email: req.body.email
+    // }
+
     try {
-        const result = await fetch(url, {
+        await fetch(url, {
             method: "POST",
             headers: headers,
             body: JSON.stringify(data),
         }).then(res => {
             return res.json()
         })
-        res.status(200).json('OK')
+        res.status(200)
     } catch (error) {
-        res.status(500).send(error)
+        res.status(500)
     }
 }
