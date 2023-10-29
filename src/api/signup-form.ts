969f74a412ec7eef4edcc5886d6d51e6bfe7e8e6
Ayush Singh <ayush.singh@evicore.com>
2023-10-29 14:21:09 -0700

    Alert on slack instead of Zapier

--- src/api/signup-form.ts
+++ src/api/signup-form.ts
@@ -15,23 +15,22 @@ export default async function handler(
 
     // POST data to Zapier who sends it to Google sheet & Slack 
     // TODO: trigger Poshmarkmapp email template send
-    const url = process.env.ZAPIER_WZ_WEBHOOK_URL!
+    const url = process.env.SLACK_WZ_WEBHOOK_URL!
     const headers = {
         // "Access-Control-Allow-Origin": "https://wuruzeka.com",
         "Content-Type": "application/json"
     }
-    // const data = {
-    //     channel: "#users",
-    //     text: `New user signed up for beta: ${req.body.email}`
-    // }
-    // https://api.slack.com/reference/surfaces/formatting#date-formatting
     const data = {
-        datetime: Date.now(),
-        date: new Date(Date.now()).toUTCString(),
-        email: req.body.email
+        text: `New user signed up for beta: ${req.body.email}`
     }
+    // https://api.slack.com/reference/surfaces/formatting#date-formatting
+    // const data = {
+    //     datetime: Date.now(),
+    //     date: new Date(Date.now()).toUTCString(),
+    //     email: req.body.email
+    // }
     try {
-        const result = await fetch(url, {
+        await fetch(url, {
             method: "POST",
             headers: headers,
             body: JSON.stringify(data),
